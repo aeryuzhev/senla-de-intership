69da2ec40a4646eb1abb7fce196165a58149f63d
aerik <aeryuzhev@outlook.com>
2023-09-12 01:46:53 +0300

    Update full_etl.sql

--- block_01/sql/full_elt.sql
+++ block_01/sql/full_elt.sql
@@ -1,4 +1,9 @@
 -- -------------------------------------------------------------------------------------
+drop materialized view if exists mv_aggregated_transactions_by_month;
+drop materialized view if exists mv_most_expensive_articles;
+drop materialized view if exists mv_most_active_month_decades;
+drop view if exists v_transactions_part_date;
+-- -------------------------------------------------------------------------------------
 drop table if exists stg_articles;
 -- -------------------------------------------------------------------------------------
 create table stg_articles (
@@ -47,7 +52,7 @@ create table stg_transactions (
     t_dat             date,
     customer_id       text,
     article_id        integer,
-    price             numeric(19, 18),
+    price             numeric(22, 20),
     sales_channel_id  smallint
 );
 -- -------------------------------------------------------------------------------------
@@ -74,78 +79,97 @@ with (
     delimiter ',',
     header true
 );
+-- --------------------------------------------------------------------------------------
+create view v_transactions_part_date as
+    select      
+        t.t_dat as transaction_date,
+        t.customer_id,
+        t.article_id,
+        t.price,
+        (date_trunc('month', t.t_dat) + interval '1 month - 1 day')::date as part_date,
+        case    
+            when extract('day' from t.t_dat) <= 10 then 1
+            when extract('day' from t.t_dat) <= 20 then 2
+            when extract('day' from t.t_dat) <= 31 then 3
+        end as month_decade
+    from
+        stg_transactions t;
 -- -------------------------------------------------------------------------------------
-copy (
-    with 
-    cte_filtered_transactions as (
+create materialized view mv_aggregated_transactions_by_month as 
+    select
+        t.part_date,
+        t.customer_id,
+        sum(coalesce(t.price, 0))::numeric(22, 20) as transaction_amount,
+        count(t.article_id) as number_of_articles,
+        count(distinct a.product_group_name) as number_of_product_groups  
+    from
+        v_transactions_part_date t
+        join stg_articles a on a.article_id = t.article_id
+    group by
+        t.part_date,
+        t.customer_id;
+-- -------------------------------------------------------------------------------------
+create materialized view mv_most_expensive_articles as 
+    with cte_ranked_articles as (
         select
-            t.t_dat as transaction_date,
-            (date_trunc('month', t.t_dat) + interval '1 month - 1 day')::date as part_date,
-            case    
-                when extract('day' from t.t_dat) <= 10 then 1
-                when extract('day' from t.t_dat) <= 20 then 2
-                when extract('day' from t.t_dat) <= 31 then 3
-            end as month_decade,
-            t.customer_id,
-            t.article_id,
-            t.price
-        from
-            stg_transactions t
-        where
-            t.t_dat >= '2018-10-01'
-            and t.t_dat <= '2018-10-31'
-    ),
-    -- -------------------------------
-    cte_prior_months_transactions as (
-        select
-            t.customer_id,
-            count(distinct extract('month' from t.t_dat)) as count_transaction_month
-        from
-            stg_transactions t
-        where
-            t.t_dat >= '2018-08-01'
-            and t.t_dat <= '2018-09-30'
-        group by
-            customer_id
-    ),    
-    -- -------------------------------
-    cte_ranked_articles as (
-        select
+            t.part_date,
             t.customer_id,
             t.article_id,
             row_number() over (
-                partition by t.customer_id 
+                partition by t.part_date, t.customer_id 
                 order by t.price desc, t.transaction_date
             ) as price_rank
         from
-            cte_filtered_transactions t
-    ),
-    -- -------------------------------
-    cte_aggregated_transactions_by_decade as (
+            v_transactions_part_date t
+    )
+    select
+        r.part_date,
+        r.customer_id,
+        r.article_id   
+    from
+        cte_ranked_articles r
+    where
+        r.price_rank = 1;
+-- -------------------------------------------------------------------------------------
+create materialized view mv_most_active_month_decades as 
+    with cte_aggregated_transactions_by_month_decade as (
         select
+            t.part_date,
             t.month_decade,
-            t.customer_id,        
-            sum(coalesce(t.price, 0)) as transaction_amount     
+            t.customer_id,
+            sum(coalesce(t.price, 0)) as transaction_amount
         from
-            cte_filtered_transactions t 
+            v_transactions_part_date t
         group by
+            t.part_date,
             t.month_decade,
-            t.customer_id        
+            t.customer_id    
     ),
-    -- -------------------------------
-    cte_ranked_transactions_by_decade as (
+    cte_ranked_month_decades as (
         select
+            t.part_date,
             t.month_decade,
             t.customer_id,
             row_number() over (
-                partition by t.customer_id
+                partition by t.part_date, t.customer_id
                 order by t.transaction_amount desc, t.month_decade
             ) as month_decade_rank      
         from
-            cte_aggregated_transactions_by_decade t 
-    ),
-    -- -------------------------------
-    cte_aggregated_transactions_by_month as (
+            cte_aggregated_transactions_by_month_decade t     
+    )
+    select
+        r.part_date,
+        r.month_decade,
+        r.customer_id
+    from
+        cte_ranked_month_decades r   
+    where
+        month_decade_rank = 1;
+-- -------------------------------------------------------------------------------------
+drop table if exists dm_transactions_by_month;
+-- -------------------------------------------------------------------------------------
+create table dm_transactions_by_month as 
+    with cte_enriched_transactions as (
         select
             t.part_date,
             t.customer_id,
@@ -153,48 +177,66 @@ copy (
                 when c.age < 23 then 'S'
                 when c.age < 60 then 'A'
                 else 'R'
-            end as customer_group_by_age,   
-            sum(coalesce(t.price, 0)) as transaction_amount,
-            count(t.article_id) as number_of_articles,
-            count(distinct a.product_group_name) as number_of_product_groups  
+            end as customer_group_by_age, 
+            t.transaction_amount,
+            count(t.customer_id) over (
+                partition by t.customer_id 
+                order by t.part_date 
+                rows between 2 preceding and 1 preceding
+            ) as prior_months_count,
+            min(t.part_date) over () as min_part_date,
+            a.article_id as most_exp_article_id,
+            t.number_of_articles,
+            t.number_of_product_groups,
+            d.month_decade as most_active_decade    
         from
-            cte_filtered_transactions t 
-            join stg_articles a on a.article_id = t.article_id
+            mv_aggregated_transactions_by_month t
             join stg_customers c on c.customer_id = t.customer_id
-        group by
-            t.part_date,
-            t.customer_id,
-            customer_group_by_age
-    ),
-    -- -------------------------------
-    cte_min_part_date as (
-        select
-            (date_trunc('month', min(st.t_dat)) + interval '1 month - 1 day')::date as min_part_date
-        from
-            stg_transactions st
-    )  
+            join mv_most_expensive_articles a on a.customer_id = t.customer_id
+                and a.part_date = t.part_date
+            join mv_most_active_month_decades d on d.customer_id = t.customer_id
+                and d.part_date = t.part_date
+    )
     select
         t.part_date,
         t.customer_id,
-        t.customer_group_by_age,
+        t.customer_group_by_age, 
         t.transaction_amount,
-        a.article_id as most_exp_article_id,
+        t.most_exp_article_id,
         t.number_of_articles,
         t.number_of_product_groups,
-        d.month_decade as most_active_decade,
+        t.most_active_decade,
         case 
-            when t.part_date = m.min_part_date then 1
-            when date_part('month', t.part_date) - date_part('month', m.min_part_date) = 1 then coalesce(p.count_transaction_month, 0)
-            when p.count_transaction_month = 2 then 1
+            when t.part_date = t.min_part_date 
+                then 1
+            when date_part('month', t.part_date) - date_part('month', t.min_part_date) = 1 
+                then coalesce(t.prior_months_count, 0)
+            when t.prior_months_count = 2 
+                then 1
             else 0
         end as customer_loyalty  
     from
-        cte_aggregated_transactions_by_month t
-        cross join cte_min_part_date m
-        join cte_ranked_articles a on a.customer_id = t.customer_id
-            and a.price_rank = 1
-        join cte_ranked_transactions_by_decade d on d.customer_id = t.customer_id
-            and d.month_decade_rank = 1
-        left join cte_prior_months_transactions p on p.customer_id = t.customer_id
-) to '/home/aerik/learning/code/senla-de-intership/block_01/data/data_mart.csv' csv header; 
+        cte_enriched_transactions t;   
+-- -------------------------------------------------------------------------------------
+drop materialized view if exists mv_aggregated_transactions_by_month;
+drop materialized view if exists mv_most_expensive_articles;
+drop materialized view if exists mv_most_active_month_decades;
+drop view if exists v_transactions_part_date;
+-- -------------------------------------------------------------------------------------
+copy (
+    select
+        t.part_date,
+        t.customer_id,
+        t.customer_group_by_age, 
+        t.transaction_amount,
+        t.most_exp_article_id,
+        t.number_of_articles,
+        t.number_of_product_groups,
+        t.most_active_decade,
+        t.customer_loyalty      
+    from
+        dm_transactions_by_month t
+    where
+        t.part_date = '2018-10-31'
+) to '/home/aerik/learning/code/senla-de-intership/block_01/data/data_mart.csv' csv header;
 -- -------------------------------------------------------------------------------------
