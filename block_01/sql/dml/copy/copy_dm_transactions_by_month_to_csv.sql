3c4ea4550d39c27df206711c851b9b6151f10501
aerik <aeryuzhev@outlook.com>
2023-09-12 01:46:23 +0300

    Update copy scripts

---
+++ block_01/sql/dml/copy/copy_dm_transactions_by_month_to_csv.sql
@@ -0,0 +1,18 @@
+-- -------------------------------------------------------------------------------------
+copy (
+    select
+        t.part_date,
+        t.customer_id,
+        t.customer_group_by_age, 
+        t.transaction_amount,
+        t.most_exp_article_id,
+        t.number_of_articles,
+        t.number_of_product_groups,
+        t.most_active_decade,
+        t.customer_loyalty      
+    from
+        dm_transactions_by_month t
+    where
+        t.part_date = '2018-10-31'
+) to '/home/aerik/learning/code/senla-de-intership/block_01/data/data_mart.csv' csv header;
+-- -------------------------------------------------------------------------------------
