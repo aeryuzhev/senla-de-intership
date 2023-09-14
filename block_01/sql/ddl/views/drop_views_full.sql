38cb6aab2a483a2db62861061252365a91460eb1
aerik <aeryuzhev@outlook.com>
2023-09-14 17:36:06 +0300

    refactor: split drop_views.sql

--- block_01/sql/ddl/views/drop_views_full.sql
+++ block_01/sql/ddl/views/drop_views_full.sql
@@ -2,5 +2,5 @@
 drop materialized view if exists mv_aggregated_transactions_by_month;
 drop materialized view if exists mv_most_expensive_articles;
 drop materialized view if exists mv_most_active_month_decades;
-drop view if exists v_transactions_part_date;
--- --------------------------------------------------------------------------------------
+drop view if exists v_transactions_part_date_full;
+-- -------------------------------------------------------------------------------------
---
+++ block_01/sql/ddl/views/drop_views_month.sql
@@ -0,0 +1,9 @@
+-- -------------------------------------------------------------------------------------
+drop view if exists v_aggregated_transactions_by_month;
+drop view if exists v_ranked_articles;
+drop view if exists v_ranked_month_decades;
+drop view if exists v_prior_months_transactions;
+drop view if exists v_min_part_date;
+drop view if exists mv_transactions_part_date;
+drop view if exists v_transactions_params;
+-- -------------------------------------------------------------------------------------
